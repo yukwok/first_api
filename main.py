0ba587c6514b91e1ede5ba0a3bb45c04cb579df0
Yukwok <billynyk@gmail.com>
2021-06-11 16:07:57 +0800

    notification email finished.

--- main.py
+++ main.py
@@ -3,61 +3,83 @@ from datetime import datetime
 import smtplib
 import time
 #
-# MY_EMAIL = "___YOUR_EMAIL_HERE____"
-# MY_PASSWORD = "___YOUR_PASSWORD_HERE___"
-MY_LAT = 51.507351 # Your latitude     # London
+MY_EMAIL = "yukwokng@yahoo.com"
+MY_PASSWORD = "owkmmhlihiurujnm"
+MY_LAT = 51.507351 # Your latitude
 MY_LONG = -0.127758 # Your longitude
 #
 #
-# def is_iss_overhead():
-#     response = requests.get(url="http://api.open-notify.org/iss-now.json")
-#     response.raise_for_status()
-#     data = response.json()
-#     print(type(data))
+def is_iss_overhead():
+    print("is over head?")
+
+    response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    response.raise_for_status()
+    data = response.json()
+    print(type(data))
+
+    iss_latitude = float(data["iss_position"]["latitude"])
+    iss_longitude = float(data["iss_position"]["longitude"])
+
+    #Your position is within +5 or -5 degrees of the iss position.
+    if MY_LAT-5 <= iss_latitude <= MY_LAT+5 and MY_LONG-5 <= iss_longitude <= MY_LONG+5:
+        return True
 #
-#     iss_latitude = float(data["iss_position"]["latitude"])
-#     iss_longitude = float(data["iss_position"]["longitude"])
 #
-#     #Your position is within +5 or -5 degrees of the iss position.
-#     if MY_LAT-5 <= iss_latitude <= MY_LAT+5 and MY_LONG-5 <= iss_longitude <= MY_LONG+5:
-#         return True
-#
-#
-# def is_night():
-print("is night time?")
-parameters = {
-    "lat": MY_LAT,
-    "lng": MY_LONG,
-    "formatted": 0,
-}
-response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
-response.raise_for_status()
-data = response.json()
-sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
-sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
-time_now = datetime.now().hour
-# print(sunrise)
-# print(time_now)
+def is_night():
+    print("is night time?")
+    parameters = {
+        "lat": MY_LAT,
+        "lng": MY_LONG,
+        "formatted": 0,
+    }
+    response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+    response.raise_for_status()
+    data = response.json()
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+    time_now = datetime.now().hour
+
+    if time_now >= sunset or time_now <= sunrise:
+        print("is night time? true")
+
+        return True
+    else:
+        print("is night time? false")
+
+        return False
+
+
+while True:
+    print("running...")
+    print(is_night())
+    print(is_iss_overhead())
+
+    time.sleep(60)
 
 
 
 
-    # if time_now >= sunset or time_now <= sunrise:
-    #     return True
+    if is_night() and is_iss_overhead() :
+        print('iss....')
+        with smtplib.SMTP("smtp.mail.yahoo.com", port=587) as connection:
+            connection.starttls()
+            connection.login(user=MY_EMAIL, password=MY_PASSWORD)
+            connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addrs="billynyk@gmail.com",
+            msg="Subject:Look Up.\n\nThe ISS is above you in the sky."
+        )
+    else:
+        print('no iss....')
+        with smtplib.SMTP("smtp.mail.yahoo.com", port=587) as connection:
+            connection.starttls()
+            connection.login(user=MY_EMAIL, password=MY_PASSWORD)
+            connection.sendmail(
+                from_addr=MY_EMAIL,
+                to_addrs="billynyk@gmail.com",
+                msg="Subject:No need to Look Up.\n\nThe ISS is above you in the sky.")
+#
 
 
-# while True:
-#     time.sleep(1)
-#     print("running...")
-#     if is_night() and is_iss_overhead() :
-#         print('iss....')
-#         connection = smtplib.SMTP("__YOUR_SMTP_ADDRESS_HERE___")
-#         connection.starttls()
-#         connection.login(MY_EMAIL, MY_PASSWORD)
-#         connection.sendmail(
-#             from_addr=MY_EMAIL,
-#             to_addrs=MY_EMAIL,
-#             msg="Subject:Look Up👆\n\nThe ISS is above you in the sky."
-#         )
-#
-#
+
+
